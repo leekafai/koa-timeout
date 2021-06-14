8ad1d67899c1eacf6918918cdfd5a5587f9be8f4
leekafai <leekafai95@outlook.com>
2021-06-14 14:58:04 +0800

    tscofig target ES2020

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,8 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const timeout = (millionSecond) => {
     const TIMEOUT = Math.random();
     const ms = +millionSecond;
-    const mdw = (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const mdw = async (ctx, next) => {
         if (!isNaN(ms) && ms >= 1) {
             let timer;
             const timeout = new Promise((resolve) => {
@@ -19,7 +10,7 @@ const timeout = (millionSecond) => {
                     resolve(TIMEOUT);
                 }, ms);
             });
-            const v = yield Promise.race([timeout, next()]);
+            const v = await Promise.race([timeout, next()]);
             clearTimeout(timer);
             if (v === TIMEOUT) {
                 ctx.status = 408;
@@ -27,9 +18,9 @@ const timeout = (millionSecond) => {
             }
         }
         else {
-            yield next();
+            await next();
         }
-    });
+    };
     return mdw;
 };
 module.exports = timeout;
